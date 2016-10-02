ce312c3d94481a3a6ed07122ae7c0cdff466986e
RamAnanth <ram.ananth14@gmail.com>
2016-10-02 18:25:37 +0530

    Added solutions route

--- app/scripts/controllers/summit.js
+++ app/scripts/controllers/summit.js
@@ -8,7 +8,14 @@
  * Controller of the frontend2017App
  */
 angular.module('frontend2017App')
-  .controller('SummitCtrl', function () {
+  .controller('SummitCtrl', function ($scope,$http) {
     skrollr.init().destroy();
-    
+  $scope.eventList=[];
+     $http.get('http://shaastra.org:8001/api/eventLists/events')
+     .then(function(response){
+     	$scope.eventList=response.data;
+    });
+      $scope.goto=function(index){
+      	$location.path('eventLists/'+$scope.eventList[index]._id);
+      }  
   });
