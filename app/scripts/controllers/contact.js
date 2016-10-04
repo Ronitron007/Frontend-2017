2f9d4b9c99866245a3e3741fe98a13eb280c15e4
Minu Aswanth <minuapple@gmail.com>
2016-10-05 00:31:38 +0530

    add contact details

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -15,28 +15,130 @@ angular.module('frontend2017App')
   });
   var list=[
 	{
-	Team_Name:"name",
-	Email:"xyz@abc.com",
-	Phone:"+91 1234567890",
+	Person_Name:"Animesh Bagora",
+	Team_Name:"Co-Curricular Affairs Secretary",
+	Email:"animesh.bagora@shaastra.org",
+	Phone:"+91 9884299639",
 	},
 	{
-	Team_Name:"name",
-	Email:"xyz@abc.com",
-	Phone:"+91 1234567890",
+	Person_Name:"Pendse Neil Sanjay",
+	Team_Name:"Concept & Design",
+	Email:" neil@shaastra.org",
+	Phone:"+91 9176283794",
 	},
 	{
-	Team_Name:"name",
-	Email:"xyz@abc.com",
-	Phone:"+91 1234567890",
+	Person_Name:"Abhijit S Gupta",
+	Team_Name:"Concept & Design",
+	Email:" abhijit@shaastra.org",
+	Phone:"+91 9940439339",
 	},
 	{
-	Team_Name:"name",
-	Email:"xyz@abc.com",
-	Phone:"+91 1234567890",
+	Person_Name:"Bharath",
+	Team_Name:"Concept & Design",
+	Email:"ae15d019@smail.iitm.ac.in",
+	Phone:"+91 9940579724",
 	},
 	{
-	Team_Name:"name",
-	Email:"xyz@abc.com",
-	Phone:"+91 1234567890",
+	Person_Name:"Harshit Dohare",
+	Team_Name:"Envisage",
+	Email:" harshit.dohare@shaastra.org",
+	Phone:"+91 9962879064",
+	},
+	{
+	Person_Name:"Omkar Vanjpe",
+	Team_Name:"Envisage",
+	Email:" omkarkvanjpe@gmail.com",
+	Phone:"+91 7708173059",
+	},{
+	Person_Name:"Adarsh",
+	Team_Name:"Events",
+	Email:"adarshg@shaastra.org",
+	Phone:"+91 9600193202",
+	},{
+	Person_Name:"Suvindu Ch",
+	Team_Name:"Events",
+	Email:"suvindu@shaastra.org",
+	Phone:"+91 9940248645",
+	},{
+	Person_Name:"Rohit Thakran",
+	Team_Name:"Events",
+	Email:"7rohitthakran@gmail.com",
+	Phone:"+91 9958515422",
+	},{
+	Person_Name:"Ramprashanth",
+	Team_Name:"Evolve",
+	Email:"ramprashanth@shaastra.org",
+	Phone:"+91 8754443260",
+	},{
+	Person_Name:"Udith Krishna",
+	Team_Name:"Evolve",
+	Email:"udithkrishna@shaastra.org",
+	Phone:"+91 9551113873",
+	},{
+	Person_Name:"Harshit Saxena",
+	Team_Name:"Facilities",
+	Email:"harshit@shaastra.org",
+	Phone:"+91 9789016167",
+	},{
+	Person_Name:"Krishi Tata",
+	Team_Name:"Finance",
+	Email:"krishi@shaastra.org",
+	Phone:"+91 9003113121",
+	},{
+	Person_Name:"L Sai Manish",
+	Team_Name:"Finance",
+	Email:"saimanish@shaastra.org",
+	Phone:"+91 7708481881",
+	},{
+	Person_Name:"Nivedita Manepalli",
+	Team_Name:"Shows and Exhibitions",
+	Email:"nivedita.manepalli@shaastra.org",
+	Phone:"+91 9940557095",
+	},
+	{
+	Person_Name:"Raghul Manosh",
+	Team_Name:"Sponsorship & PR",
+	Email:"raghul@shaastra.org",
+	Phone:"+91 9940246479",
+	},{
+	Person_Name:"Varun Srinivas Sridharan",
+	Team_Name:"Sponsorship & PR",
+	Email:"varun@shaastra.org",
+	Phone:"+91 7358540260",
+	},{
+	Person_Name:"M Aghamarshana",
+	Team_Name:"Media & Student Relations",
+	Email:"aghamarshana@shaastra.org",
+	Phone:"+91 8122495355",
+	},{
+	Person_Name:" R Rathna Kumar",
+	Team_Name:"Media & Student Relations",
+	Email:"rathnakumar@shaastra.org",
+	Phone:"+91 9677081113",
+	},{
+	Person_Name:"Hitesh Singla",
+	Team_Name:"Media & Student Relations",
+	Email:"hitesh@shaastra.org",
+	Phone:"+91 9677043890",
+	},{
+	Person_Name:"Mohit Kumar",
+	Team_Name:"Technical Operations",
+	Email:"mhtkmr54@gmail.com",
+	Phone:"+91 9003120598",
+	},{
+	Person_Name:"S.V.Minu Aswanth",
+	Team_Name:"Technical Operations",
+	Email:"minu@shaastra.org",
+	Phone:"+91 9976448668",
+	},{
+	Person_Name:"Manish Dhariwal",
+	Team_Name:"QMS",
+	Email:"manish.dhariwal@shaastra.org",
+	Phone:"+91 9884184032",
+	},{
+	Person_Name:"Gunjan",
+	Team_Name:"QMS",
+	Email:"bmsw.gnp@gmail.com",
+	Phone:"+91 7299559744",
 	}
 	];
