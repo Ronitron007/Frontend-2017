ee1a319174ba4504df2ace1eecde6ef9e39a852a
ojasvita <ojasvitawalgad@gmail.com>
2016-10-07 18:41:37 +0530

    frontend changes

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -11,26 +11,20 @@ angular.module('frontend2017App')
   .controller('NavbarCtrl', function ($scope, Auth, $location,$anchorScroll) {
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser = Auth.getCurrentUser;
-   $scope.showDown = true;
-    $scope.showUp = false;
+   
     $scope.logout = function () {
       Auth.logout();
       $location.path('/login');
     };
 
-    $scope.gotoBottom = function() {
-      if( $scope.showDown == true)
-      {
-      $location.hash('d');
-      }
-      else
-      {
-      $location.hash('u');
-      }
-      $anchorScroll();
-      $scope.showDown = $scope.showDown ? false : true;
-      $scope.showUp = $scope.showUp ? false : true;
+     $scope.hoverIn = function(){
+        this.hoverEdit = true;
     };
+
+    $scope.hoverOut = function(){
+        this.hoverEdit = false;
+    };
+    
      
 	  
 
