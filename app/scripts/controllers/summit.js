4413c56d8857239739e8504cd96aa0634da8af9d
RamAnanth <ram.ananth14@gmail.com>
2016-10-28 08:16:22 +0530

    updated summit page

--- app/scripts/controllers/summit.js
+++ app/scripts/controllers/summit.js
@@ -18,7 +18,7 @@ angular.module('frontend2017App')
   					{name:'registrations',img:imgURL+'REGISTRATION.png'},
   					{name:'partners',img:imgURL+'PARTNERS.png'},
   					{name:'past-versions',img:imgURL+'PAST_VERSIONS.png'},
-            {name:'faq',img:'../../images/elements/placeholder.png'},
+            {name:'faq',img:imgURL+'FAQ.png'},
   					];
     
                                
