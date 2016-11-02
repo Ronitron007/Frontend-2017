c92ae804bdf58b4abdb732208adc0bdc2dfa30a9
RamAnanth <ram.ananth14@gmail.com>
2016-11-02 20:50:12 +0530

    minoinor changes

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -8,10 +8,13 @@
  * Controller of the frontend2017App
  */
 angular.module('frontend2017App')
-  .controller('NavbarCtrl', function ($scope,$http, Auth, $location,$anchorScroll) {
+  .controller('NavbarCtrl', function ($scope,$routeParams,$http, Auth, $location,$anchorScroll) {
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser = Auth.getCurrentUser;
-   
+    
+    var eventId = $routeParams.eventId;
+    $scope.eventsJSON = [];
+    
     $scope.logout = function () {
       Auth.logout();
       $location.path('/login');
@@ -25,15 +28,43 @@ angular.module('frontend2017App')
         this.hoverEdit = false;
     };
 
-    $http.get('http://shaastra.org:8001/api/events/forSearch')
+
+      
+    // $http.get('http://shaastra.org:8001/api/eventLists/events/' + eventId)
+    //     .then(function (response) {
+    //       $scope.eventLists = response.data;
+    //       $scope.eventListContent = response.data.info;
+
+    //       if(((response.data.events.length)%2) === 0){
+    //         $scope.normal = true;
+    //       }
+
+    //       else{
+    //         $scope.normal = false;
+    //       }
+          
+    //       $scope.eventLists.title = $scope.eventLists.title.split(" ").join("-");
+
+    //       var num = response.data.events.length;
+    //         for(var i=0; i<num; i++) {
+    //           if(response.data.events[i].acceptedByAdmin === false) {
+    //             response.data.events[i].imageURL = 'http://shaastra.org:8001/api/uploads/' + response.data.events[i].imageid + '/' + response.data.events[i].imagename;
+    //             $scope.eventsJSON.push(response.data.events[i]);
+    //           }
+    //         }
+    //       });
+  
+  $http.get('http://shaastra.org:8001/api/events/forSearch')
     .then(function(response){
       $scope.eventNames=[{name:String,index:Number}];
+      $scope.eventIds=[];
       $scope.events=response.data;
       for (var i in $scope.events) {
         $scope.eventNames.push({name:$scope.events[i].name,index:i});
-      console.log($scope.eventNames[i].name)
-      }
-    })
+        $scope.eventIds.push($scope.events[i]._id);
+
+    }
+  });
     
      
 	  
