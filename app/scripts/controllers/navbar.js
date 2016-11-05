1f5dd318bfad9e8e4d133ff2f0107ec1021bd8dd
RamAnanth <ram.ananth14@gmail.com>
2016-11-05 11:04:58 +0530

    Added search bar functionality

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -12,8 +12,8 @@ angular.module('frontend2017App')
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.getCurrentUser = Auth.getCurrentUser;
     
-    var eventId = $routeParams.eventId;
-    $scope.eventsJSON = [];
+    
+   
     
     $scope.logout = function () {
       Auth.logout();
@@ -28,44 +28,30 @@ angular.module('frontend2017App')
         this.hoverEdit = false;
     };
 
-
+   
       
-    // $http.get('http://shaastra.org:8001/api/eventLists/events/' + eventId)
-    //     .then(function (response) {
-    //       $scope.eventLists = response.data;
-    //       $scope.eventListContent = response.data.info;
+   
 
-    //       if(((response.data.events.length)%2) === 0){
-    //         $scope.normal = true;
-    //       }
+ 
 
-    //       else{
-    //         $scope.normal = false;
-    //       }
-          
-    //       $scope.eventLists.title = $scope.eventLists.title.split(" ").join("-");
 
-    //       var num = response.data.events.length;
-    //         for(var i=0; i<num; i++) {
-    //           if(response.data.events[i].acceptedByAdmin === false) {
-    //             response.data.events[i].imageURL = 'http://shaastra.org:8001/api/uploads/' + response.data.events[i].imageid + '/' + response.data.events[i].imagename;
-    //             $scope.eventsJSON.push(response.data.events[i]);
-    //           }
-    //         }
-    //       });
-  
   $http.get('http://shaastra.org:8001/api/events/forSearch')
     .then(function(response){
       $scope.eventNames=[{name:String,index:Number}];
       $scope.eventIds=[];
+      $scope.eventListIds=[];
+
       $scope.events=response.data;
+      
       for (var i in $scope.events) {
+        
         $scope.eventNames.push({name:$scope.events[i].name,index:i});
         $scope.eventIds.push($scope.events[i]._id);
-
-    }
+        $scope.eventListIds.push($scope.events[i].eventCategory[0]);
+         }
+          
   });
-    
+   
      
 	  
 
--- app/scripts/controllers/summit-partners.js
+++ app/scripts/controllers/summit-partners.js
@@ -10,14 +10,14 @@
 angular.module('frontend2017App')
   .controller('SummitPartnersCtrl', function ($scope) {
     skrollr.init().destroy();
-    ;
+    
     $scope.partnersJson=[
     {
     	name:'Accessible India Campaign',
     	info:'A nation-wide campaign started by the Department of Empowerment of Persons with Disability (DEPwD), Government of India, aimed at achieving universal accessibility for Persons with Disabilities (PwDs). The campaign focusses on making physical environments accessible ranging from buildings to transport facilities provided by the government and making information and communication technology accessible for PwDs.',
     	link:'www.accessibleindia.gov.in/content/',
     	logo:'a',
-    };
+    },
 
 
 
