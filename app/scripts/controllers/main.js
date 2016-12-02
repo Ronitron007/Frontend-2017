76e6513e0bd757401db43bccfb7def08ff8eaecc
Minu Aswanth <minuapple@gmail.com>
2016-12-02 11:00:19 +0530

    commented out global notifs for now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,19 +10,20 @@
 angular.module('frontend2017App')
   .controller('MainCtrl', function ($scope, $interval, notify) {
 
-  	notify.config({startTop: 60, maximumOpen: 1})
-    var notificationMessages = [];
-  	var count = notificationMessages.length - 1;
-  	$scope.notifyFunction = function(){
-  		notify({ message: notificationMessages[sessionStorage.notifCount], duration:4000, position: "left"});	
-  		sessionStorage.notifCount++;
-  	}
-  	if(sessionStorage.notifCount == 1){
-  		notify({ message: notificationMessages[0], duration:4000, position: "left"});
-  	}
-  	if(sessionStorage.notifCount < count){
-  		$interval($scope.notifyFunction, 4000, count);
-  	}
+    //Notifications ready, use when needed
+  	// notify.config({startTop: 60, maximumOpen: 1})
+   //  var notificationMessages = [];
+  	// var count = notificationMessages.length - 1;
+  	// $scope.notifyFunction = function(){
+  	// 	notify({ message: notificationMessages[sessionStorage.notifCount], duration:4000, position: "left"});	
+  	// 	sessionStorage.notifCount++;
+  	// }
+  	// if(sessionStorage.notifCount == 1){
+  	// 	notify({ message: notificationMessages[0], duration:4000, position: "left"});
+  	// }
+  	// if(sessionStorage.notifCount < count){
+  	// 	$interval($scope.notifyFunction, 4000, count);
+  	// }
   });
 
 // var myApp = angular.module("myApp", ["sn.skrollr"]);
